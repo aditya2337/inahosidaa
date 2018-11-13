30e370a4fb36d293d1bdfa3b257aeab680bdebc4
Aditya <androiditya@gmail.com>
2018-11-13 15:26:16 +0530

    js to ts

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="h-100 w-100">
+        hi
+      </div>
+    );
+  }
+}
+
+export default App;
